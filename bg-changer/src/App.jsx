cc6710c97eb9d271c3b8f9d15f5274712a5d01bb
kunal <kunal20110907@gmail.com>
2025-08-15 16:16:49 +0530

    make bg-changer

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -3,17 +3,28 @@ import './index.css'
 
 
 function App() {
+    let [color, setcolor] = useState("olive")
+  // console.log(document.querySelectorAll("button"))
+    // const buttons=document.querySelectorAll("button")
+
+    // buttons.forEach(element => {
+    // element.addEventListener('click',()=>{
+    //   alert("background color : "+element.style.backgroundColor)
+    //   document.body.style.backgroundColor=element.style.backgroundColor;
+    // });
+  // })
+
   return (
-    <>
-      <div className='w-full h-full bg-black flex flex-col justify-center align-middle' >
-        <button className='w-15 bg-red-600'>red</button>
-        <button className='bg-blue-600'>blue</button>
-        <button className='bg-green-600'>green</button>
-        <button className='bg-orange-600'>orange</button>
-        <button className='bg-white'>white</button>
-        <button className='bg-black'>black</button>
+    <div className='h-screen w-full' style={{backgroundColor:color}}>
+      <div className='w-20 absolute right-10 top-20 rounded-lg border-transparent h-100 bg-blue-100 flex flex-col justify-around items-center' >
+        <button onClick={()=>setcolor("red")} className='w-15 cursor-pointer rounded border-transparent text-white' style={{backgroundColor:"red"}}>red</button>
+        <button onClick={()=>setcolor("blue")} className='w-15 cursor-pointer rounded border-transparent text-white ' style={{backgroundColor:"blue"}}>blue</button>
+        <button onClick={()=>setcolor("green")} className='w-15 cursor-pointer rounded border-transparent text-white ' style={{backgroundColor:"green"}}>green</button>
+        <button onClick={()=>setcolor("orange")} className='w-15 cursor-pointer rounded border-transparent text-white ' style={{backgroundColor:"orange"}}>orange</button>
+        <button onClick={()=>setcolor("white")} className='w-15 cursor-pointer rounded border-transparent text-black ' style={{backgroundColor:"white"}}>white</button>
+        <button onClick={()=>setcolor("black")} className='w-15 cursor-pointer rounded border-transparent text-white ' style={{backgroundColor:"black"}}>black</button>
       </div>
-    </>
+    </div>
   )
 }
 
